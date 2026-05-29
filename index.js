bbb9d5aa3027c0cce96648a1719b2c41f818dd2b
ZachTheMighty <promoted66@gmail.com>
2026-05-29 22:58:40 +0000

    Changed: block object to an IIFE
    Added:
    object representing the gameboard

--- index.js
+++ index.js
@@ -1,26 +1,61 @@
 function gameboard() {
   const blocks = [];
-}
 
-function block() {
-  let state = "empty";
-  let id;
+  (() => {
+    for (let i = 0; i < 9; i++)
+      blocks.push(
+        (() => {
+          let state = "empty";
 
-  function setCross() {
-    state = "cross";
+          function setCross() {
+            state = "cross";
+            displayBoard();
+          }
+
+          function setNought() {
+            state = "nought";
+            displayBoard();
+          }
+
+          function emptyState() {
+            state = "empty";
+            displayBoard();
+          }
+
+          function getState() {
+            return state;
+          }
+
+          return { setCross, setNought, getState, emptyState };
+        })(),
+      );
+  })();
+
+  function playCross(blockNum) {
+    if (blocks[blockNum - 1].getState() === "empty")
+      blocks[blockNum - 1].setCross();
   }
 
-  function setNought() {
-    state = "nought";
+  function playNought(blockNum) {
+    if (blocks[blockNum - 1].getState() === "empty")
+      blocks[blockNum - 1].setNought();
   }
 
-  function emptyState() {
-    state = "empty";
+  function displayBoard() {
+    for (i = 0; i < 9; i++) {
+      const state = blocks[i].getState();
+
+      if (state === "empty") process.stdout.write("*");
+
+      if (state === "cross") process.stdout.write("X");
+
+      if (state === "nought") process.stdout.write("O");
+
+      if ((i !== 0 && i === 2) || i === 5 || i === 8)
+        process.stdout.write("\n");
+    }
+    console.log();
   }
 
-  function getState() {
-    return state;
-  }
-
-  return { setCross, setNought, getState, emptyState };
+  return { playCross, playNought, displayBoard };
 }
